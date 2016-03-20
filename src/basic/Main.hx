9a93d638e8fc1a367bcd8dc4e41786053502fc0e
Adam Faur <adam.faur@gmail.com>
2016-03-20 12:43:41 -0500

    Add results window.

--- src/basic/Main.hx
+++ src/basic/Main.hx
@@ -20,21 +20,36 @@ class Main {
 		});
 
 		var mainWindow = null;
+		var resultsWindow = null;
 
 		App.on( ready, function() {
 
+			var width  = Math.floor(800 / 2);
+			var height = Math.floor(600 / 2);
+
 			mainWindow = new BrowserWindow({
+				x: width, y: height, center: false,
 				frame: false, show: false, resizable: false,
 				width: 600, height: 70
 			});
 			mainWindow.loadURL( 'file://' + __dirname + '/app.html' );
 			mainWindow.on( closed, function() mainWindow = null );
 
+			resultsWindow = new BrowserWindow({
+				x: width, y: (height + 70), center: false,
+				frame: false, show: false, resizable: false,
+				width: 600, height: 200
+			});
+			resultsWindow.loadURL( 'file://' + __dirname + '/results.html' );
+			resultsWindow.on( closed, function() resultsWindow = null );
+
 			GlobalShortcut.register('ctrl+x', function() {
 				if (mainWindow.isVisible()) {
 					mainWindow.hide();
+					resultsWindow.hide();
 				} else {
 					mainWindow.show();
+					resultsWindow.show();
 				}
 			});
 
